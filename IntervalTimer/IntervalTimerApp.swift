6aefa1156eef4809695938b96527f23daea88787
Michael LeBlanc <leblanc.michael@gmail.com>
2024-06-09 20:33:36 -0400

    Initial Commit

---
+++ IntervalTimer/IntervalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalTimerApp.swift
+//  IntervalTimer
+//
+//  Created by Michael LeBlanc on 2024-06-09.
+//
+
+import SwiftUI
+
+@main
+struct IntervalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
